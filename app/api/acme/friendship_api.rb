ea562432d7b9526e701ec6066f845413b523cdd0
Daniel Le <greenrecyclebin@gmail.com>
2017-10-07 12:44:38 +0800

    Refactor common friendship fetching code

--- app/api/acme/friendship_api.rb
+++ app/api/acme/friendship_api.rb
@@ -43,34 +43,7 @@ module Acme
           user_email = params[:friends].first
           friend_email = params[:friends].last
 
-          user = User.find_by(email: user_email)
-          friend = User.find_by(email: friend_email)
-
-          column_name = 'common_friend_id'
-
-          query = <<~SQL
-          SELECT friendships1.friend_id AS #{column_name}
-          FROM (
-            SELECT *
-            FROM friendships
-            WHERE user_id = $1
-          ) friendships1 INNER JOIN (
-            SELECT *
-            FROM friendships
-            WHERE user_id = $2
-          ) friendships2
-            ON friendships1.friend_id = friendships2.friend_id
-          SQL
-
-          common_friend_ids =
-            if user && friend
-              result = ActiveRecord::Base.connection.raw_connection.execute(query, [user.id, friend.id])
-              result.map { |row| row[column_name] }
-            else
-              []
-            end
-
-          common_friends = User.where(id: common_friend_ids)
+          common_friends = Friendship.common(user_email: user_email, friend_email: friend_email)
 
           {success: true, friends: common_friends.map(&:email), count: common_friends.size}
         end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -21,4 +21,32 @@ class Friendship < ApplicationRecord
       none
     end
   end
+
+  def self.common(user_email:, friend_email:)
+    user = User.find_by(email: user_email)
+    friend = User.find_by(email: friend_email)
+
+    return User.none unless user && friend
+
+    column_name = 'common_friend_id'
+
+    query = <<~SQL
+    SELECT friendships1.friend_id AS #{column_name}
+    FROM (
+      SELECT *
+      FROM friendships
+      WHERE user_id = $1
+    ) friendships1 INNER JOIN (
+      SELECT *
+      FROM friendships
+      WHERE user_id = $2
+    ) friendships2
+      ON friendships1.friend_id = friendships2.friend_id
+    SQL
+
+    result = ActiveRecord::Base.connection.raw_connection.execute(query, [user.id, friend.id])
+    common_friend_ids = result.map { |row| row[column_name] }
+
+    User.where(id: common_friend_ids)
+  end
 end
