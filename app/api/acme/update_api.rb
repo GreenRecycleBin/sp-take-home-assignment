ea9d99203a532bdeb43a849136a499299ba948e9
Daniel Le <greenrecyclebin@gmail.com>
2017-10-07 22:54:43 +0800

    Add "POST /api/update/block"

--- app/api/acme/update_api.rb
+++ app/api/acme/update_api.rb
@@ -1,13 +1,27 @@
 module Acme
   class UpdateAPI < Grape::API
-    resource :update do
-      params do
+    helpers do
+      params :requestor_and_target do
         requires :requestor, type: String, valid_email: true, fail_fast: true
         requires :target, type: String, valid_email: true, fail_fast: true
+      end
+    end
+
+    resource :update do
+      params do
+        use :requestor_and_target
       end
 
       post do
         {success: true}
+      end
+
+      namespace :block do
+        params do
+          use :requestor_and_target
+        end
+
+        post {}
       end
     end
   end
--- spec/api/update_api_spec.rb
+++ spec/api/update_api_spec.rb
@@ -124,4 +124,116 @@ describe Acme::UpdateAPI do
       end
     end
   end
+
+  describe 'block' do
+    describe 'POST' do
+      context 'without params[:requestor]' do
+        subject { post '/api/update/block' }
+
+        it 'returns 400' do
+          subject
+
+          expect(response).to have_http_status(:bad_request)
+        end
+
+        it 'returns an error' do
+          subject
+
+          expect(response.body).to eq({error: 'requestor is missing'}.to_json)
+        end
+      end
+
+      context 'without params[:target]' do
+        subject { post '/api/update/block', params: {requestor: 'a@example.com'} }
+
+        it 'returns 400' do
+          subject
+
+          expect(response).to have_http_status(:bad_request)
+        end
+
+        it 'returns an error' do
+          subject
+
+          expect(response.body).to eq({error: 'target is missing'}.to_json)
+        end
+      end
+
+      context 'with params[:requestor]' do
+        subject { post '/api/update/block', params: {requestor: requestor} }
+
+        context 'when not given an email address' do
+          let(:requestor) { [] }
+
+          it 'returns 400' do
+            subject
+
+            expect(response).to have_http_status(:bad_request)
+          end
+
+          it 'returns an error' do
+            subject
+
+            expect(response.body).to eq({error: 'requestor is invalid'}.to_json)
+          end
+        end
+
+        context 'when given an invalid email address' do
+          let(:requestor) { 'invalid email' }
+
+          it 'returns 400' do
+            subject
+
+            expect(response).to have_http_status(:bad_request)
+          end
+
+          it 'returns an error' do
+            subject
+
+            expect(response.body).to eq({error: 'requestor must be a valid email address.'}.to_json)
+          end
+        end
+
+        context 'when given an email address' do
+          context 'with params[:target]' do
+            subject { post '/api/update/block', params: {requestor: requestor, target: target} }
+
+            let(:requestor) { 'a@example.com' }
+
+            context 'when not given an email address' do
+              let(:target) { [] }
+
+              it 'returns 400' do
+                subject
+
+                expect(response).to have_http_status(:bad_request)
+              end
+
+              it 'returns an error' do
+                subject
+
+                expect(response.body).to eq({error: 'target is invalid'}.to_json)
+              end
+            end
+
+            context 'when given an invalid email address' do
+              let(:target) { 'invalid email' }
+
+              it 'returns 400' do
+                subject
+
+                expect(response).to have_http_status(:bad_request)
+              end
+
+              it 'returns an error' do
+                subject
+
+                expect(response.body).to eq({error: 'target must be a valid email address.'}.to_json)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
